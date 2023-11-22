2ea318060e63ce79ce0e22ad58a2d71998a8892a
Colin Fahrion <colin.fahrion@gmail.com>
2023-11-21 18:21:24 -0800

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+export default class WebComponent extends HTMLElement {
+	private shadow: ShadowRoot;
+
+	constructor() {
+		super();
+		this.shadow = this.attachShadow({ mode: "open" });
+	}
+
+	static get observedAttributes(): string[] {
+		return ["foo"];
+	}
+
+	get foo(): string {
+		return this.getAttribute("foo") || "";
+	}
+	set foo(value) {
+		if (typeof value === "string") this.setAttribute("foo", value);
+	}
+
+	public connectedCallback(): void {
+		this.render();
+	}
+
+	public attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+		console.log("changed", name, oldValue, newValue);
+	}
+
+	private render(): void {
+		const html = `<div><slot></slot></div>`;
+		const css = `<style></style>`;
+
+		this.shadow.innerHTML = `${css}${html}`;
+	}
+}
+customElements.define("web-component", WebComponent);
